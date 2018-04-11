4b08e6c20c5081ae51f5fabe321afa7c268e4d5f
karmab <karimboumedhel@gmail.com>
2018-04-11 10:26:39 +0200

    handle tsb in cache cleaning

--- clean_service_catalog.sh
+++ clean_service_catalog.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 oc get clusterserviceclass --no-headers | awk '{ print $1 }' | xargs oc delete clusterserviceclass
 oc get clusterserviceplan --no-headers | awk '{ print $1 }' | xargs oc delete clusterserviceplan
-
 oc get bundles --no-headers | xargs oc delete bundles
-
 apb bootstrap
 apb relist
+oc delete pod --all -n openshift-template-service-broker
--- helpers/delete_windowsvm.sh
+++ helpers/delete_windowsvm.sh
@@ -3,4 +3,4 @@ oc delete ovm $1 -n $NAMESPACE
 oc delete route http-$1 -n $NAMESPACE
 oc delete svc http-$1 -n $NAMESPACE
 oc delete svc rdp-$1 -n $NAMESPACE
-oc delete pvc $1 -n $NAMESPACE
+#oc delete pvc $1 -n $NAMESPACE
