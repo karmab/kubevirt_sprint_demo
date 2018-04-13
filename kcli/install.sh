8b1f8ed75008fc888493d1027d3a7639f9117eb1
karmab <karimboumedhel@gmail.com>
2018-04-13 12:53:37 +0200

    provide default users

--- kcli/install.sh
+++ kcli/install.sh
@@ -7,7 +7,4 @@ ssh-keyscan -H master.[[ domain ]] >> ~/.ssh/known_hosts
 sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
-[% for master in range(0, masters) %]
-ssh root@master0[[ master + 1 ]].[[ domain ]] htpasswd -b /etc/origin/master/htpasswd [[ user ]] [[ password ]]
-[% endfor %]
 oc adm policy add-cluster-role-to-user cluster-admin developer
