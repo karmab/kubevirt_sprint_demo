ef628cd00552c3d791183a9a8b6bb8f4ea753373
karmab <karimboumedhel@gmail.com>
2018-04-17 00:46:44 +0200

    update kcli plan

--- kcli/install.sh
+++ kcli/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-yum -y install openshift-ansible screen
+yum -y install openshift-ansible screen bind-utils
 [% for master in range(0, masters) %]
 ssh-keyscan -H master0[[ master + 1 ]].[[ domain ]] >> ~/.ssh/known_hosts
 [% endfor %]
-ssh-keyscan -H master.[[ domain ]] >> ~/.ssh/known_hosts
+export IP=`dig +short master01.[[ domain ]]`
 sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
+sed -i "s/openshift_master_default_subdomain=.*/openshift_master_default_subdomain=$IP.xip.io/" /root/hosts
+#sed -i "s/openshift_master_cluster_hostname=.*/openshift_master_cluster_hostname=$IP.xip.io/" /root/hosts
+#sed -i "s/openshift_master_cluster_public_hostname=.*/openshift_master_cluster_public_hostname=$IP.xip.io/" /root/hosts
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
-oc adm policy add-cluster-role-to-user cluster-admin developer
