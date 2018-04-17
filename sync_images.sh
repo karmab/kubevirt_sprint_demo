21c6090dbb5b0ceb410e9eeb42a74fe825900032
karmab <karimboumedhel@gmail.com>
2018-04-18 01:20:57 +0200

    use latest kubevirt images

--- sync_images.sh
+++ sync_images.sh
@@ -5,7 +5,7 @@
 HUB="docker.io"
 ORG="mutism"
 TAG="demo"
-IMAGES="ansibleplaybookbundle/kubevirt-apb jniederm/origin-web-console:demo-v9 ansibleplaybookbundle/mssql-apb:latest docker.io/ansibleplaybookbundle/kubevirt-apb:latest kubevirt/virt-api:v0.4.1-alpha.1 kubevirt/virt-controller:v0.4.1-alpha.1 kubevirt/virt-handler:v0.4.1-alpha.1 kubevirt/virt-launcher:v0.4.1-alpha.1 gluster/heketiclone:latest jcoperh/importer:latest ansibleplaybookbundle/import-vm-disk-apb:latest ansibleplaybookbundle/import-vm-apb:latest"
+IMAGES="ansibleplaybookbundle/kubevirt-apb jniederm/origin-web-console:demo-v9 ansibleplaybookbundle/mssql-apb:latest docker.io/ansibleplaybookbundle/kubevirt-apb:latest kubevirt/virt-api:v0.4.1 kubevirt/virt-controller:v0.4.1 kubevirt/virt-handler:v0.4.1 kubevirt/virt-launcher:v0.4.1 gluster/heketiclone:latest jcoperh/importer:latest ansibleplaybookbundle/import-vm-disk-apb:latest ansibleplaybookbundle/import-vm-apb:latest ansibleplaybookbundle/windows-vm-apb:latest"
 for image in $IMAGES ; do
   echo "Processing $image"
   id=`docker images $image -q`
