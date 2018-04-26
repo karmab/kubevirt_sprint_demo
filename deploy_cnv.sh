bf5f49d88f16e260a339acf2aef5184a42133305
root <root@cnv1.cloud.lab.eng.bos.redhat.com>
2018-04-26 09:06:27 +0000

    pubkey handling

--- deploy_cnv.sh
+++ deploy_cnv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 URL="https://download.fedoraproject.org/pub/fedora/linux/releases/27/CloudImages/x86_64/images/Fedora-Cloud-Base-27-1.6.x86_64.qcow2"
 PUBKEY=`cat ~/.ssh/id_rsa.pub`
-ansible-playbook -i /root/hosts -e "name=fedora type=linux url=$URL public_key=\"$PUBKEY\" namespace=default preload=true" deploy.yml
+ansible-playbook -i /root/hosts -e "name=fedora type=linux url=$URL public_key=\"$PUBKEY\" namespace=default sync=false" deploy.yml
